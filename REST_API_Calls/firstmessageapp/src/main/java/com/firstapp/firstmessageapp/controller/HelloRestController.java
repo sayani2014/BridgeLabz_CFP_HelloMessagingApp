20348bedcfcf370ea28d50f25104be5fef644855
Sayani Koley <sayanikoley8@gmail.com>
2021-08-07 14:51:46 +0530

    Ability to make REST Call to show Hello Sayani!

--- REST_API_Calls/firstmessageapp/src/main/java/com/firstapp/firstmessageapp/controller/HelloRestController.java
+++ REST_API_Calls/firstmessageapp/src/main/java/com/firstapp/firstmessageapp/controller/HelloRestController.java
@@ -1,8 +1,8 @@
 /**
- * UC1 : Create a Rest Controller to demonstrate the various HTTP Methods and respond hello messages to the User.
- *       To begin with show Hello from BridgeLabz
+ * UC2 : Make REST Call to show Hello Mark from BridgeLabz
+ *          - Use GET Request Method and pass name as query parameter
  *          - Use CURL to demonstrate the REST API Call
- *          - curl localhost:8080/hello -w "\n"
+ *          - curl localhost:8080/getMessage?name=Sayani -w "\n"
  *
  * @author : SAYANI KOLEY
  * @since : 07.08.2021
@@ -10,14 +10,17 @@
 
 package com.firstapp.firstmessageapp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestController {
 
     /**
-     * Purpuse : Once the request mapping takes place in the server, the sayHello() is called and the output message is
+     * Annotation for mapping web requests onto methods in request-handling classes with flexible method signatures.
+     * Purpuse : Once the mapping takes place in the server, the sayHello() is called and the output message is
                  displayed as output.
      * @return
      */
@@ -25,5 +28,18 @@ public class HelloRestController {
     @RequestMapping(value = "/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    /**
+     * Annotation for mapping HTTP GET requests onto specific handler methods.
+     * Purpuse : Once the mapping takes place in the server, the sayHello() is called and the output message is
+                 displayed as output.
+     * @param name
+     * @return
+     */
+
+    @GetMapping(value = "/getMessage")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 }
